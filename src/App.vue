ce0fed83fba217078b943add16316d698a1e2114
Laissy <laissydominique@gmail.com>
2024-07-17 13:29:12 -0300

    configurando index

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,10 @@ const info = ref({
 });
 
 async function getData() {
-
-  const response =
-    await fetch(`https://rickandmortyapi.com/api/character/?name=${info.value.personagem}`);
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/?name=${info.value.personagem}`
+  );
   const data = await response.json();
-  // console.log(data);
 
   for (const item of data.results) {
     info.value.dados.push({
@@ -24,7 +23,6 @@ async function getData() {
       origem: item.origin.name,
       status: item.status,
     });
-
   }
 }
 </script>
@@ -33,49 +31,46 @@ async function getData() {
   <div class="container">
     <h1>Pesquise o personagem</h1>
 
-  <div class="trazer_info"> 
-  <div class="input"> 
-  <input v-model="info.personagem" type="text" class="input" />
-</div>
-  <div class="btn">
-    <button @click="getData">Clique</button>
-  </div>
-</div>
-<div class="conteudo"> 
-  <div v-for="personagem in info.personagens" :key="personagem.personagem">
-    <p>{{ personagem.personagem }}</p>
-  </div>
+    <div class="trazer_info">
+      <div class="input">
+        <input v-model="info.personagem" type="text" class="input" />
+      </div>
+      <div class="btn">
+        <button @click="getData">Clique</button>
+      </div>
+    </div>
+    <div class="conteudo">
+      <div v-for="personagem in info.personagens" :key="personagem.personagem">
+        <p>{{ personagem.personagem }}</p>
+      </div>
 
-  <div class="personagens" v-for="item in info.dados" :key="item.genero">
-    <div class="personagem"> 
-    <p class="nome" > {{ item.personagem }}</p>
+      <div class="personagens" v-for="item in info.dados" :key="item.genero">
+        <div class="personagem">
+          <p class="nome">{{ item.personagem }}</p>
+        </div>
+        <div class="img">
+          <img :src="item.img" alt="" />
+        </div>
+        <div class="infos">
+          <div class="genero">
+            <p id="genero"><span> Gênero:</span> {{ item.genero }}</p>
+          </div>
+          <div class="especie">
+            <p><span> Espécie:</span> {{ item.especie }}</p>
+          </div>
+          <div class="local">
+            <p><span> Local:</span> {{ item.local }}</p>
+          </div>
+          <div class="origem">
+            <p><span> Origem:</span> {{ item.origem }}</p>
+          </div>
+          <div class="status">
+            <p><span> Status:</span> {{ item.status }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <div class="img"> 
-    <img :src="item.img" alt="">
-  </div>
-  <div class="infos"> 
-
-  <div class="genero"> 
-    <p id="genero" > <span> Gênero:</span>  {{ item.genero }}</p>
-  </div>
-  <div class="especie"> 
-    <p><span> Espécie:</span> {{ item.especie }}</p>
-  </div>
-  <div class="local"> 
-    <p><span> Local:</span> {{ item.local }}</p>
-  </div>
-  <div class="origem">
-    <p><span> Origem:</span> {{ item.origem }}</p>
-
-  </div>
-  <div class="status"> 
-    <p><span> Status:</span> {{ item.status }}</p>
-  </div>
-  </div>
-  </div>
-</div>
-</div>
-
 </template>
 
 <style scoped></style>
